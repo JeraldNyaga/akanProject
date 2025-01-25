db424b420d28e8bcfcafe09bf688a2b519389e0c
judgeOstrich <jeraldnyaga15@gmail.com>
2025-01-25 14:25:31 +0300

    finish logic of akanName generator

--- scripts/akanLogic.js
+++ scripts/akanLogic.js
@@ -1,11 +1,14 @@
 const maleAkanNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
 const femaleAkanNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
 
-let date = 24;
+
+let date = 25;
 let month = 1;
 let year = 2025;
 // 0 represents Male and 1 represents female
 let gender = 0;
+let indexOfDay = getDayOfWeek();
+console.log(indexOfDay)
 
 //formula 
 
@@ -13,8 +16,11 @@ let gender = 0;
 
 function getDayOfWeek() {
     const CC = Math.floor(year / 100);
-    const YY = year % 100;
-    const MM = month;
+    let monthYear = monthAlteration(checkWhetherLeap(2025));
+    let updatedMonth = Number(monthYear[0]);
+    let updatedYear = Number(monthYear[1]);
+    const YY = updatedYear % 100;
+    const MM = updatedMonth;
     const DD = date;
 
     const indexOfDay = Math.floor(((CC / 4 - (2 * CC) -1 ) + (5 * YY / 4) + (26 * (MM + 1) / 10) + DD) % 7);
@@ -26,7 +32,7 @@ function checkWhetherLeap(userYear){
 
     userYear -= 1;
     if ((userYear % 4 == 0 && userYear % 100 != 0) || userYear % 400 == 0) {
-      return ture;
+      return true;
     } else {
       return false;
     }
@@ -34,18 +40,28 @@ function checkWhetherLeap(userYear){
 }
 
 function monthAlteration(){
-    if (checkWhetherLeap() && MM == 1 || 2){
-        MM += 12;
-        YY -= 1;
+    if (checkWhetherLeap() && month == 1 || 2){
+        month += 12;
+        year -= 1;
 
-        return [MM, YY]
+        return [month, year]
     } else{
-        return [MM, YY]
+        return [month, year]
     }
 }
 
 function getAkanName(){
-
+    let akanName = null;
+    if (gender == 0){
+        akanName = maleAkanNames[indexOfDay];
+        return akanName
+    } else if(gender == 1){
+        akanName = femaleAkanNames[indexOfDay];
+        return akanName;
+    } else{
+        return 'Invalid name';
+    }
 }
-let CC = Math.floor(year / 100)
-console.log(getDayOfWeek())
+
+
+console.log(getAkanName())
