80544dd6b0ec92d46c6645b7478c0d0640737517
judgeOstrich <jeraldnyaga15@gmail.com>
2025-01-25 15:35:55 +0300

    FIXBUG on akanLogix

--- scripts/akanLogic.js
+++ scripts/akanLogic.js
@@ -1,14 +1,13 @@
-const maleAkanNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
-const femaleAkanNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
+const maleAkanNames = ['Kwame','Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi']
+const femaleAkanNames = ['Ama','Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua']
 
 
-let date = 15;
-let month = 8;
-let year = 1995;
+let date = 25;
+let month = 12;
+let year = 2001;
 // 0 represents Male and 1 represents female
-let gender = 1;
+let gender = 0;
 let indexOfDay = getDayOfWeek();
-console.log(indexOfDay)
 
 //formula 
 
@@ -16,47 +15,46 @@ console.log(indexOfDay)
 
 function getDayOfWeek() {
     const CC = Math.floor(year / 100);
-    let monthYear = monthAlteration(checkWhetherLeap(2025));
-    let updatedMonth = Number(monthYear[0]);
-    let updatedYear = Number(monthYear[1]);
-    const YY = updatedYear % 100;
-    const MM = updatedMonth;
+    // let monthYear = monthAlteration(checkWhetherLeap(2025));
+    // let updatedMonth = Number(monthYear[0]);
+    // let updatedYear = Number(monthYear[1]);
+    const YY = year % 100;
+    const MM = month;
     const DD = date;
 
-    const indexOfDay = Math.floor(((CC / 4 - (2 * CC) -1 ) + (5 * YY / 4) + (26 * (MM + 1) / 10) + DD) % 7);
+    const indexOfDay =(DD + Math.floor((13*(MM + 1))/5) + YY + Math.floor((YY / 4)) + Math.floor((CC / 4)) - (2*CC)) % 7
 
-    return indexOfDay;
+    return (indexOfDay + 7) % 7;
 }
 
-function checkWhetherLeap(userYear){
+// function checkWhetherLeap(userYear){
 
-    userYear -= 1;
-    if ((userYear % 4 == 0 && userYear % 100 != 0) || userYear % 400 == 0) {
-      return true;
-    } else {
-      return false;
-    }
+//     userYear -= 1;
+//     if ((userYear % 4 == 0 && userYear % 100 != 0) || userYear % 400 == 0) {
+//       return true;
+//     } else {
+//       return false;
+//     }
 
-}
+// }
 
-function monthAlteration(){
-    if (checkWhetherLeap() && month == 1 || 2){
-        month += 12;
-        year -= 1;
+// function monthAlteration(){
+//     if (checkWhetherLeap() && month == 1 || 2){
+//         month += 12;
+//         year -= 1;
 
-        return [month, year]
-    } else{
-        return [month, year]
-    }
-}
+//         return [month, year]
+//     } else{
+//         return [month, year]
+//     }
+// }
 
-function getAkanName(){
-    let akanName = null;
+function getAkanName(indexOfDayWeek){
     if (gender == 0){
-        akanName = maleAkanNames[indexOfDay];
+        akanName = maleAkanNames[indexOfDayWeek];
         return akanName
     } else if(gender == 1){
-        akanName = femaleAkanNames[indexOfDay];
+        akanName = femaleAkanNames[indexOfDayWeek];
         return akanName;
     } else{
         return 'Invalid name';
@@ -64,4 +62,4 @@ function getAkanName(){
 }
 
 
-console.log(getAkanName())
+console.log(getAkanName(indexOfDay))
