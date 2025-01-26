84de26da2160a2b56307aa49ca75d6f33dfaa525
judgeOstrich <jeraldnyaga15@gmail.com>
2025-01-26 20:36:10 +0300

    Test AkanLogic

--- scripts/akanLogic.js
+++ scripts/akanLogic.js
@@ -1,6 +1,7 @@
 const maleAkanNames = ['Kwame','Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi']
 const femaleAkanNames = ['Ama','Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua']
 const thirtyMonthday = [4, 6, 9, 11];
+const daysOfWeek = ["Saturday","Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
 
 // 0  means Male
 // 1 means female
@@ -21,13 +22,13 @@ function getDayOfWeek() {
 
     return (indexOfDay + 7) % 7;
 }
-function getAkanName(indexOfDayWeek){
+function getAkanName(){
     if (gender == 0){
-        akanName = maleAkanNames[indexOfDayWeek];
-        return akanName
+        akanName = maleAkanNames[indexOfDay];
+        return showResult(akanName, validateMonthInput(year, month, date), indexOfDay)
     } else if(gender == 1){
-        akanName = femaleAkanNames[indexOfDayWeek];
-        return akanName;
+        akanName = femaleAkanNames[indexOfDay];
+        return showResult(akanName, validateMonthInput(year, month, date), indexOfDay);
     } else{
         return 'Invalid name';
     }
@@ -55,5 +56,13 @@ function validateMonthInput(year, month, date){
     }
     else{
         return true
+    }
+}
+
+function showResult(akanName, validation, indexOfDay){
+    if(validation){
+        let nameAkan = akanName;
+        let dayBorn = daysOfWeek[indexOfDay];
+        console.log(`Your Akan name is ${nameAkan}, you were born on ${dayBorn}`)
     }
 }
